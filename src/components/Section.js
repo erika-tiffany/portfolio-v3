402ca66cf8dddfd79f94997451ccc72a8428531a
Erika K <erikatiffany.k@gmail.com>
2020-03-27 16:03:58 +0900

    Add a new section component that uses render props to send down visibility prop to children components.

---
+++ src/components/Section.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import VisibilitySensor from "react-visibility-sensor";
+
+export const Section = (props) => {
+  const [visible, setVisible] = useState(false);
+
+  function onChange(isVisible) {
+    // if visible is already set to true, we don't care anymore
+    // because it means the section has already been viewed
+    // and therefore the content is already in view
+    if (visible === true) return;
+    setVisible(isVisible);
+  }
+
+  return (
+    <section className="section section--full-viewport">
+      <VisibilitySensor onChange={ onChange }>
+        { props.render(visible) }
+      </VisibilitySensor>
+    </section>
+  );
+}
