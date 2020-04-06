9ce342f16fb4d9e3637988beda124b29e5c5376a
Erika K <erikatiffany.k@gmail.com>
2020-04-07 06:16:12 +0900

    Technology tags list component for project page

---
+++ src/components/Project/TechnologyTag.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+export const TechnologyTag = (props) => {
+  const [animated, setAnimated] = useState(false);
+
+  useEffect(() => {
+    if (props.visible && animated === false) {
+      const timer = setTimeout(() => {
+        setAnimated(true);
+        if (props.last) props.onFinishAnimating();
+      }, props.order * 100);
+      return () => clearTimeout(timer);
+    }
+  }, [animated, props]);
+
+  return (
+    <li className={ 'project-intro__tag ' + (animated ? 'animated fadeIn' : 'hidden') } >{ props.tag }</li>
+  );
+}
---
+++ src/components/Project/TechnologyTagsList.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import VisibilitySensor from "react-visibility-sensor";
+import { TechnologyTag } from './TechnologyTag';
+
+export const TechnologyTagsList = (props) => {
+  const [visible, setVisible] = useState(false);
+  const [finishedAnimating, setFinishedAnimating] = useState(false);
+
+  function handleVisibilityChange(isVisible) {
+    // if visible is already set to true, we don't care anymore
+    // because it means the section has already been viewed
+    // and therefore the content is already in view
+    if (visible === true) return;
+    setVisible(isVisible);
+  }
+
+  function handleFinishedAnimating() {
+    if (finishedAnimating === true) return;
+    setFinishedAnimating(true);
+  }
+
+  return (
+    <VisibilitySensor onChange={ handleVisibilityChange }>
+      <>
+        <ul className="project-intro__tags">
+          { props.tags.map((tag, key) => (
+            <TechnologyTag key={ key } 
+                           visible={ visible } 
+                           tag={ tag } 
+                           order={ key }
+                           last={ (props.tags.length - 1) <= key }
+                           onFinishAnimating={ handleFinishedAnimating } />
+          )) }
+        </ul>
+        <hr className={ 'project-intro__divider' + (finishedAnimating ? ' project-intro__divider-show' : '') } />
+      </>
+    </VisibilitySensor>
+  );
+}
